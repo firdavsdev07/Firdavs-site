897aba4d8b3570218325d7447f4a9b84fd2a6b3c
Firdavs-Normurodov <firdavsnormurodov435@gmail.com>
2025-02-27 20:17:30 +0500

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/logo.png" sizes="any" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="shortcut icon" href="/logo.png" type="image/x-icon" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
